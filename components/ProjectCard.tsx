76716c7509f9c0e830855f14905676eae0aee2f3
Dinne Kopelevich <dinne22@hotmail.com>
2023-12-27 23:14:03 -0700

    Create compoments

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+interface Props {
+    image: string;
+    title: string;
+    text: string;
+}
+
+const ProjectCard = ({ image, title, text}: Props) => {
+  
+  return (
+    <div>
+      Place Holders
+    </div>
+  )
+}
+
+export default ProjectCard
