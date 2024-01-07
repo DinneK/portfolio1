27359d3d8ddf81e75f1daf2b876945322b97dc8a
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-07 15:21:34 -0700

    add fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,15 @@
-import {Inter} from 'next/font/google'
+import {Inter, Dr_Sugiyama, Poiret_One} from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin']})
+
+export const drSugiyama = Dr_Sugiyama({ 
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+export const poiretOne = Poiret_One({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+
