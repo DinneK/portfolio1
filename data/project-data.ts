1772cdf2eef32082a4ba97e3fe76bbfa24c27e59
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-08 13:50:35 -0700

    Add project data

---
+++ data/project-data.ts
@@ -0,0 +1,32 @@
+export const Projects = [
+  {
+    title: "Stripe Payment Implementation",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/stripe-landing.png",
+  },
+  {
+    title: "FieldTrippers",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/fieldtrippers-landing.png",
+  },
+  {
+    title: "The Art of the Cocktail",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/art-cocktail-landing.png",
+  },
+  {
+    title: "D.E.D. Library",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/ded-landing.png",
+  },
+  {
+    title: "GitFit",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/gitfit-landing.png",
+  },
+  {
+    title: "Travel Tracker",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/travel-landing.png",
+  },
+];
