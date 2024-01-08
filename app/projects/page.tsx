c229dbd308d5e73d84e670215757888c1b758fc8
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-08 13:16:39 -0700

    Add project images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { drSugiyama } from '../ui/fonts'
+
 export default function Page() {
-  return <p>Projects Page</p>
+  return (
+    <main className="flex flex-grow items-center p-6 h-screen bg-gradient-to-b from-fuchsia-300 to-black h-screen">
+      <div className='flex flex-col flex-grow items-center'>
+        <h2 className='text-[40px] text-white'>PROJECTS</h2>
+        <h3 className='text-[30px] text-white text-wrap'>Some fascinating things about my work and projects</h3>
+      </div>
+      <div className='flex-grow grid grid-rows-4 grid-flow-col gap-20'>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+        <div className='size-36 border-red-400 border-solid border-2'>
+          <h1 className='text-white'>Projects</h1>
+        </div>
+      </div>
+    </main>
+  )
 }
--- app/ui/navigation-bar/sidenav.tsx
+++ app/ui/navigation-bar/sidenav.tsx
@@ -1,8 +1,28 @@
+'use client'
+
+import { usePathname } from "next/navigation";
 import NavLinks from "./nav-links";
+import clsx from "clsx";
 
 export default function SideNav() {
+  const path = usePathname()
+  const isLightPage = (path === '/' || path === '/about');
+
+  const sidebarClasses = clsx(
+    'flex',
+    'flex-col',
+    'px-3',
+    'py-4',
+    'md:px-2',
+    {
+      'bg-gradient-to-b from-fuchsia-300 to-white': isLightPage,
+      'text-white bg-gradient-to-b from-fuchsia-300 to-black': !isLightPage, 
+    },
+     'h-screen'
+  );
+
   return (
-    <div className="flex flex-col px-3 py-4 md:px-2 bg-gradient-to-b from-fuchsia-300 to-white h-screen">
+    <div className={sidebarClasses}>
       <NavLinks />
     </div>
   )
