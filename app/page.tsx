03456f5af11f317e31c7fe244f4fc04383067ec1
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-07 17:30:14 -0700

    Add links to projects, about, contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { drSugiyama } from './ui/fonts';
 
 export default function Home() {
   return (
-    <main className="flex-grow p-6 h-screen bg-gradient-to-b from-fuchsia-300 to-white h-screen">
-    {/* </main><main className='w-screen h-screen relative bg-cover bg-gradient-to-b from-fuchsia-300 to-white h-screen'> */}
-      <h2>Dinne Kopelevich</h2>
-      <h3>Sofware Developer</h3>
-      <h1>Projects</h1>
-      <h1>About</h1>
-      <h1>Contact</h1>
+    <main className="flex flex-grow items-center p-6 h-screen bg-gradient-to-b from-fuchsia-300 to-white h-screen">
+      <div className='flex flex-col flex-grow items-center'>
+        <h2 className='text-[60px]'>DINNE KOPELEVICH</h2>
+        <h3 className='text-[55px]'>Sofware Developer</h3>
+      </div>
+      <div className='flex flex-col flex-grow items-start'>
+        <Link
+          href={'/projects'}
+        >
+          <div className={`${drSugiyama.className} text-[150px] text-black-800`}>Projects</div>
+        </Link>
+        <Link
+          href={'/about'}
+        >
+          <div className={`${drSugiyama.className} text-[150px] text-black-800`}>About</div>
+        </Link>
+        <Link
+          href={'/contact'}
+        >
+          <div className={`${drSugiyama.className} text-[150px] text-black-800`}>Contact</div>
+        </Link>
+      </div>
     </main>
   )
 }
