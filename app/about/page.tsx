f88ad7598425e4860cd34b3126660c8e73a7d3e9
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-09 19:45:23 -0700

    Implement skills icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,30 @@
+'use client'
+import Image from "next/image"
+import { drSugiyama } from "../ui/fonts"
+import { SkillsData } from "../../data/tech-stack-data"
+
 export default function Page() {
   return (
     <main className="flex flex-grow items-center p-6 h-screen bg-gradient-to-b from-fuchsia-300 to-white h-screen">
-      <div>
-        <div>I'm a Photo</div>
+      <div className="flex flex-grow flex-col">
+        <div><img className="w-[300px] h-[300px] rounded-lg" src="/selfie.jpg" alt="A picture of me" /></div>
         <h2>Opening Line</h2>
         <h3>Some other amazing things about me</h3>
+        <h3>CV</h3>
       </div>
-      <div>
-        <h1>Tech Stack</h1>
-        <div>All the little techs</div>
+      <div className="flex flex-grow flex-col">
+        <h1 className={`${drSugiyama.className} text-[50px]`}>Tech Stack & Tools</h1>
+        <div className='grid grid-cols-5 gap-5 max-w-[90%] max-h-[90%]'>
+        {SkillsData.map((skill, index) => 
+          <Image
+            key={index}
+            src={`/skill-images${skill.Image}`}
+            alt={skill.name}
+            width={skill.width}
+            height={skill.height}
+          />
+        )}
+        </div>
       </div>
     </main>
   )
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import Link from 'next/link'
+// import Image from 'next/image'
+// import Link from 'next/link'
 // import { drSugiyama } from '../ui/fonts'
 import ProjectCard from '../ui/project-card'
 import { Projects } from '../../data/project-data'
