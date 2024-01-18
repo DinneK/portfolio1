0f02811bcee00e10f5ef5b0e5f8fb73c327eeec9
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-18 14:31:51 -0700

    Add sloth to tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,14 @@ import SideNav from './ui/navigation-bar/sidenav'
 export const metadata: Metadata = {
   title: 'Portfolio',
   description: 'This is my portfolio',
-}
+  icons: {
+    icon: [
+      {
+        url: '/sloth.png',
+      },
+    ],
+  },
+  }
 
 export default function RootLayout({
   children,
