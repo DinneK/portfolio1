a8e83b60dc9c4582d26f3099e11084912ac53197
Dinne Kopelevich <dinne22@hotmail.com>
2024-01-18 14:56:27 -0700

    Darken place holder text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,12 +19,10 @@ export default function Page() {
         <p className="text-1xl my-3 md:text-2xl md:mr-8">As a software developer, I have a keen eye for UX/UI design and have developed the ability to create applications that are both aesthetically pleasing and easy to use. My diverse skill set and experience working on various teams have given them a unique perspective on software development, and I am adept at identifying and resolving complex problems in my work.</p>
         
         <p className="text-1xl my-3 md:text-2xl md:mr-8">In addition to my technical skills, I am a creative thinker and enjoy taking on new challenges. I am constantly seeking to learn and grow, and am always looking for ways to improve my skills and contribute to the success of a team. With my experience, skills, and passion for software development, I will be a valuable asset to your organization.</p>
-
-        <button type="button" className="w-1/4 text-gray-900 bg-white border shadow-md focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-          <a  href='/cv.pdf' target="_blank"  rel="noopener noreferrer">
-            CV
-          </a>
-        </button>
+        
+        <a className="w-1/4 text-center text-gray-900 bg-white border shadow-md focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"  href='/cv.pdf' target="_blank"  rel="noopener noreferrer">
+          CV
+        </a>
       </div>
       <div className="flex flex-grow flex-col md:w-2/5 md:ml-20">
         <p className={`${drSugiyama.className} text-[50px] mt-6`}>Tech Stack & Tools</p>
